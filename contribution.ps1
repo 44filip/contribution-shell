7596633b3668946fe1438ef8b3149984c2f314b6
S-Filip <100999946+44filip@users.noreply.github.com>
2024-02-16 03:09:43 +0100

    Adding variable values

--- contribution.ps1
+++ contribution.ps1
@@ -1,3 +1,3 @@
-$repoDir
-$commitMessage
-$remoteUrl
+$repoDir = "D:\GitHub\contribution-shell"
+$commitMessage = "Automatic commit from Task Scheduler"
+$remoteUrl = "https://github.com/44filip/contribution-shell.git"
