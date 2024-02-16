f93019bd06287d644393a4c9bae864e7cdca27ab
S-Filip <100999946+44filip@users.noreply.github.com>
2024-02-16 03:29:33 +0100

    File gen init | better coding practices
    Initializing creation of empty files and changing code around to be more understandable and utilize better coding practices

--- contribution.ps1
+++ contribution.ps1
@@ -2,14 +2,26 @@ $repoDir = "D:\GitHub\contribution-shell"
 $commitMessage = "Automatic commit from Task Scheduler"
 $remoteUrl = "https://github.com/44filip/contribution-shell.git"
 
+# Text file generation
+$dateTime = Get-Date -Format "dd/MM/yyyy"
+$filePath = "$repoDir\text_files\$dateTime.txt"
+
+# (Redundant) backup case for missing path
+if (!(Test-Path -Path $filePath -PathType Directory)) {
+    New-Item -ItemType Directory -Path $filePath -Force
+}
+
+# Creating the empty text file
+New-Item -ItemType File -Path $filePath -Force
+
 # Navigating to the directory
-Start-Process cmd -ArgumentList "/c cd \"$repoDir\"" -Wait
+Set-Location -Path $repoDir
 
 # Checking for uncommitted changes
 $changes = git status --porcelain
 if ($changes) {
     # Add all the uncommitted changes, then commit, and push
-    git add -a
+    git add -.
     git commit -m "$commitMessage"
     git push -u "$remoteUrl" main
 }
