a4db3f7833cf3d35f3d8abffab09b80b9a3abe0b
S-Filip <100999946+44filip@users.noreply.github.com>
2024-02-16 03:53:37 +0100

    Setting new order

--- contribution.ps1
+++ contribution.ps1
@@ -1,29 +1,33 @@
-$repoDir = "D:\GitHub\contribution-shell"
-$commitMessage = "Automatic commit from Task Scheduler"
-$remoteUrl = "https://github.com/44filip/contribution-shell.git"
-
-# Text file generation
-$dateTime = Get-Date -Format "dd/MM/yyyy"
-$filePath = "$repoDir\text_files\$dateTime.txt"
-
 # (Redundant) backup case for missing path
 if (!(Test-Path -Path $filePath -PathType Directory)) {
     New-Item -ItemType Directory -Path $filePath -Force
 }
 
+$repoDir = "D:\GitHub\contribution-shell"
+$commitMessage = "Automatic commit from Task Scheduler"
+
+# Text file generation
+$dateTime = Get-Date -Format "dd/MM/yyyy"
+$filePath = Join-Path $repoDir "text_files\$dateTime.txt"
+
 # Creating the empty text file
 New-Item -ItemType File -Path $filePath -Force
 
-# Navigating to the directory
+# Navigating to the repository directory
 Set-Location -Path $repoDir
 
 # Checking for uncommitted changes
 $changes = git status --porcelain
 if ($changes) {
     # Add all the uncommitted changes, then commit, and push
-    git add -.
-    git commit -m "$commitMessage"
-    git push -u "$remoteUrl" main
+    git add .
+    git commit -m $commitMessage
+
+    # Determine the remote name dynamically
+    $remoteName = (git remote -v | Select-String -Pattern "(\S+)\s+https:\/\/github\.com" | ForEach-Object { $_.Matches.Groups[1].Value })[0]
+
+    # Push to the determined remote
+    git push -u $remoteName main
 }
 else {
     Write-Host "No changes to commit."
