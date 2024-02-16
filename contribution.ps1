dbd04292f76692ef3f85341a8dad9f9f7fea1841
S-Filip <100999946+44filip@users.noreply.github.com>
2024-02-16 03:12:44 +0100

    Uncommitted changes check
    If there are uncommitted changes, they are then committed with the default commit message

--- contribution.ps1
+++ contribution.ps1
@@ -4,3 +4,11 @@ $remoteUrl = "https://github.com/44filip/contribution-shell.git"
 
 # Navigating to the directory
 Start-Process cmd -ArgumentList "/c cd \"$repoDir\"" -Wait
+
+# Checking for uncommitted changes
+$changes = git status --porcelain
+if ($changes) {
+    # Add all the uncommitted changes, then commit
+    git add -a
+    git commit -m "$commitMessage"
+}
