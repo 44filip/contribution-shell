705eb3ae1da8de6548f289393d0c9fc7b6ec809a
S-Filip <100999946+44filip@users.noreply.github.com>
2024-02-16 03:11:11 +0100

    Navigating to the project directory

--- contribution.ps1
+++ contribution.ps1
@@ -1,3 +1,6 @@
 $repoDir = "D:\GitHub\contribution-shell"
 $commitMessage = "Automatic commit from Task Scheduler"
 $remoteUrl = "https://github.com/44filip/contribution-shell.git"
+
+# Navigating to the directory
+Start-Process cmd -ArgumentList "/c cd \"$repoDir\"" -Wait
