d814907f5c6752e25becff3a735aa7f34868d70d
S-Filip <100999946+44filip@users.noreply.github.com>
2024-02-16 03:14:35 +0100

    Adding missing push component
    Forgot to add git push

--- contribution.ps1
+++ contribution.ps1
@@ -8,7 +8,11 @@ Start-Process cmd -ArgumentList "/c cd \"$repoDir\"" -Wait
 # Checking for uncommitted changes
 $changes = git status --porcelain
 if ($changes) {
-    # Add all the uncommitted changes, then commit
+    # Add all the uncommitted changes, then commit, and push
     git add -a
     git commit -m "$commitMessage"
+    git push -u origin main 
+}
+else {
+    Write-Host "No changes to commit."
 }
